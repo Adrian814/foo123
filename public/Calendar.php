9bddf11d9418c2a7b7e90fb19d0e01e3f7c5492d
Name <email@email.com>
2012-03-29 15:41:26 +0200

    fifth commit

--- public/Calendar.php
+++ public/Calendar.php
@@ -1,15 +1,5 @@
 <?php
 
-if (!function_exists('cal_days_in_month')) 
-{ 
-    function cal_days_in_month($calendar, $month, $year) 
-    { 
-        return date('t', mktime(0, 0, 0, $month, 1, $year)); 
-    } 
-} 
-if (!defined('CAL_GREGORIAN')) 
-    define('CAL_GREGORIAN', 1);
-
 /**
  * Description of Calendar
  *
@@ -155,5 +145,15 @@ class Calendar {
 
         $cal = new Calendar();
         $cal->drawCalendar($month, $year);
+
+		if (!function_exists('cal_days_in_month')) 
+		{ 
+		    function cal_days_in_month($calendar, $month, $year) 
+		    { 
+		        return date('t', mktime(0, 0, 0, $month, 1, $year)); 
+		    } 
+		} 
+		if (!defined('CAL_GREGORIAN')) 
+		    define('CAL_GREGORIAN', 1);
 
 ?>

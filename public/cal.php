41d271a0cf65f413bdaebdf8e9091b727b31c484
Name <email@email.com>
2012-03-29 15:38:27 +0200

    forth commit

--- public/cal.php
+++ public/cal.php
@@ -1,5 +1,15 @@
 <?php
 
+if (!function_exists('cal_days_in_month')) 
+{ 
+    function cal_days_in_month($calendar, $month, $year) 
+    { 
+        return date('t', mktime(0, 0, 0, $month, 1, $year)); 
+    } 
+} 
+if (!defined('CAL_GREGORIAN')) 
+    define('CAL_GREGORIAN', 1);
+
         date_default_timezone_set('Europe/Paris');
 
 

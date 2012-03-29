e01eab0d07e0decdebf578a885cd119158475732
Name <email@email.com>
2012-03-29 15:47:37 +0200

    seventh commit

--- public/Calendar.php
+++ public/Calendar.php
@@ -9,15 +9,10 @@
  */
 class Calendar {
 
-	if (!function_exists('cal_days_in_month')) 
+	function cal_days_in_month($calendar, $month, $year) 
 	{ 
-	    function cal_days_in_month($calendar, $month, $year) 
-	    { 
-	        return date('t', mktime(0, 0, 0, $month, 1, $year)); 
-	    } 
+	return date('t', mktime(0, 0, 0, $month, 1, $year)); 
 	} 
-	if (!defined('CAL_GREGORIAN')) 
-	    define('CAL_GREGORIAN', 1);
 
     function Calendar()
     {
